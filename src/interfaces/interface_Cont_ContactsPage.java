180fe1172df2d93dd5ef6e1d8123be04b37290ef
giang-hai-nguyen <hoaphung90@gmail.com>
2015-11-27 14:41:50 +0700

    add interface main, new, edit for Contacts

--- src/interfaces/interface_Cont_ContactsPage.java
+++ src/interfaces/interface_Cont_ContactsPage.java
@@ -1,6 +1,7 @@
 package interfaces;
 
 public class interface_Cont_ContactsPage {
+	//toolbar button
 	public static final String new_button = "//li[@id='toolbar-new']";
 	public static final String edit_button = "//li[@id='toolbar-edit']";
 	public static final String publish_button = "//li[@id='toolbar-publish']";
@@ -9,5 +10,21 @@ public class interface_Cont_ContactsPage {
 	public static final String checkin_button = "//li[@id='toolbar-checkin']";
 	public static final String trash_button = "//li[@id='toolbar-trash']";
 	public static final String help_button = "//li[@id='toolbar-help']";
+	
+	//search option
 	public static final String filter_textbox = "//input[@id='filter_search']";
+	public static final String search_button ="//button[text()='Search']";
+	public static final String clear_button ="//button[text()='Clear']";
+	
+	//filter dropdown
+	public static final String state_dropdown = "//select[@name='filter_published']";
+	public static final String category_dropdown = "//select[@name='filter_category_id']";
+	public static final String access_dropdown = "//select[@name='filter_access']";
+	public static final String language_dropdown = "//select[@name='filter_language']";
+	
+	//paging option
+	public static final String paging_dropdown = "//select[@id='limit']";
+	
+	//table adminlist
+	
 }
--- src/interfaces/interface_Cont_EditContPage.java
+++ src/interfaces/interface_Cont_EditContPage.java
@@ -1,13 +1,13 @@
 package interfaces;
 
-public class interface_Cont_NewContPage {
-	
+public class interface_Cont_EditContPage {
 	//toolbar button
 	public static final String save_button = "//li[@id='toolbar-apply']";
 	public static final String save_close_button = "//li[@id='toolbar-save']";
 	public static final String save_new_button = "//li[@id='toolbar-save-new']";
+	public static final String save_copy_button = "//li[@id='toolbar-save-copy']";
 	public static final String cancel_button = "//li[@id='toolbar-cancel']";
-	
+		
 	//Information option
 	public static final String name_textbox = "//input[@id='jform_name']";
 	public static final String alias_textbox = "//input[@id='jform_alias']";
@@ -16,6 +16,5 @@ public class interface_Cont_NewContPage {
 	public static final String access_dropdown = "//select[@id='jform_access']";
 	public static final String language_dropdown = "//select[@id='jform_language']";
 	public static final String featured_dropdown = "//select[@id='jform_featured']";
-	public static final String image_button = "//div[@class='image']";
-	
+	public static final String image_button = "//div[@class='image']";	
 }
