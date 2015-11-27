61504586b945b4044d4c90f96fdd37c6a792b56c
giang-hai-nguyen <hoaphung90@gmail.com>
2015-11-27 13:51:20 +0700

    commit 1

---
+++ src/interfaces/interface_Cont_ContactsPage.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class interface_Cont_ContactsPage {
+	public static final String new_button = "//li[@id='toolbar-new']";
+	public static final String edit_button = "//li[@id='toolbar-edit']";
+	public static final String publish_button = "//li[@id='toolbar-publish']";
+	public static final String unpublish_button = "//li[@id='toolbar-unpublish']";
+	public static final String archive_button = "//li[@id='toolbar-archive']";
+	public static final String checkin_button = "//li[@id='toolbar-checkin']";
+	public static final String trash_button = "//li[@id='toolbar-trash']";
+	public static final String help_button = "//li[@id='toolbar-help']";
+	public static final String filter_textbox = "//input[@id='filter_search']";
+}
