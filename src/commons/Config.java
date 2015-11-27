0d3d0887a1788c6dfad3a74716c3165117f2b71e
tuan.nguyen <nbaotuan@gmail.com>
2015-11-27 18:21:58 +0700

    commit new day 2 - new common config and new testsuite_acticle

---
+++ src/commons/Config.java
@@ -0,0 +1,8 @@
+package commons;
+
+public class Config {
+	//username & password of default site 
+	public static final String default_username ="Test01";
+	public static final String default_password ="12345678";
+	public static final String default_site ="http://192.168.190.135:8888/Joomla_1.6.0/administrator";
+}
---
+++ src/testsuite_Articles/TC_JOOMLA_ARTICLE_001.java
@@ -0,0 +1,33 @@
+package testsuite_Articles;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import actions.ac_AdministratorPage;
+import actions.ac_LoginPage;
+import commons.Config;
+
+public class TC_JOOMLA_ARTICLE_001 extends commons.AutoElements {
+	@BeforeClass
+	public void Setup() {
+		driver = openAUT();
+		LoginPage = new ac_LoginPage(driver);
+		LoginPage.Login(Config.default_username, Config.default_password);
+	}
+	@Test
+	public void TO_JOOMLA_ARTICLE_001(){
+		//open new article
+	}
+	@AfterClass
+	public void teardown(){
+		AdminPage = new ac_AdministratorPage(driver);
+		AdminPage.Logout();		
+		driver.quit();
+	}
+	
+	private WebDriver driver;
+	private ac_LoginPage LoginPage;	
+	private ac_AdministratorPage AdminPage;
+}
