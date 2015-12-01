bf5a0373e3428f67cd51ec982c0172bcc8c0734a
giang-hai-nguyen <hoaphung90@gmail.com>
2015-12-01 18:58:06 +0700

    add iframe xpath

--- src/interfaces/int_NewArtPage.java
+++ src/interfaces/int_NewArtPage.java
@@ -7,6 +7,7 @@ public class int_NewArtPage {
 	public static final String access_dropdown ="//select[@id='jform_access']";
 	public static final String save_button ="//div[@id='toolbar']//span[@class='icon-32-apply']";
 	public static final String saveclose_button="//li[@id='toolbar-save']/a/span";
+	public static final String arttext_frame_textbox="//iframe[@id='jform_articletext_ifr']";
 	public static final String body_frame_textbox="//body[@id='tinymce']";
 	
 }
