15b476395d009356ba7a9ee1d53730df9e9a953d
hang.tran <thuhang09cntt2@gmail.com>
2015-12-01 18:44:53 +0700

    update interface

--- src/interfaces/int_ArticlesPage.java
+++ src/interfaces/int_ArticlesPage.java
@@ -3,14 +3,14 @@ package interfaces;
 public class int_ArticlesPage {
 	
 	
-	public static final String new_icon = "//li[@id='toolbar-new']/a/span";
+	public static final String button = "//li[@id='toolbar-new']/a/span";
 	public static final String edit_icon= "//li[@id='toolbar-edit']/a/span";
-	public static final String publish_icon= "//li[@id='toolbar-publish']/a/span";
-	public static final String unpublish_icon= "//li[@id='toolbar-unpublish']/a/span";
-	public static final String archive_icon= "//li[@id='toolbar-archive']/a/span";
-	public static final String checkin_icon= "//li[@id='toolbar-checkin']/a/span";
-	public static final String trash_icon= "//li[@id='toolbar-trash']/a/span";
-	public static final String help_icon= "//li[@id='toolbar-help']/a/span";
+	public static final String publish_button= "//li[@id='toolbar-publish']/a/span";
+	public static final String unpublish_button= "//li[@id='toolbar-unpublish']/a/span";
+	public static final String archive_button= "//li[@id='toolbar-archive']/a/span";
+	public static final String checkin_button= "//li[@id='toolbar-checkin']/a/span";
+	public static final String trash_button= "//li[@id='toolbar-trash']/a/span";
+	public static final String help_button= "//li[@id='toolbar-help']/a/span";
 	
 	
 	public static final String filter_textbox = "//input[@id='filter_search']";
