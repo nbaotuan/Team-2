a88f581da169193f51a1bb8f7b0e0aecc0fcc949
hang.tran <thuhang09cntt2@gmail.com>
2015-12-01 17:35:48 +0700

    create new interface

--- src/interfaces/int_ArticlesPage.java
+++ src/interfaces/int_ArticlesPage.java
@@ -1,5 +1,12 @@
 package interfaces;
 
 public class int_ArticlesPage {
-
+	public static final String new_icon = "//li[@id='toolbar-new']/a/span";
+	public static final String edit_icon= "//li[@id='toolbar-edit']/a/span";
+	public static final String publish_icon= "//li[@id='toolbar-publish']/a/span";
+	public static final String unpublish_icon= "//li[@id='toolbar-unpublish']/a/span";
+	public static final String archive_icon= "//li[@id='toolbar-archive']/a/span";
+	public static final String checkin_icon= "//li[@id='toolbar-checkin']/a/span";
+	public static final String trash_icon= "//li[@id='toolbar-trash']/a/span";
+	public static final String help_icon= "//li[@id='toolbar-help']/a/span";
 }
