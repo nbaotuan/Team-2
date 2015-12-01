645e0982ffe4154681efc34b27e4228393a12414
hang.tran <thuhang09cntt2@gmail.com>
2015-12-01 17:06:16 +0700

    update xpath of login button

--- src/interfaces/int_LoginPage.java
+++ src/interfaces/int_LoginPage.java
@@ -3,5 +3,5 @@ package interfaces;
 public class int_LoginPage {
 	public static final String username_textbox = "//input[@name='username']";
 	public static final String password_textbox = "//input[@name='passwd']";
-	public static final String login_button = "//div[@class='next']";
+	public static final String login_button = "//a[contains(text(),'Log in')]";
 }
