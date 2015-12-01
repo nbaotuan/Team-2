e326f58b36144c538deadc0b4b136cf7743f19d1
hang.tran <thuhang09cntt2@gmail.com>
2015-12-01 18:36:41 +0700

    update status dropdown

--- src/interfaces/int_NewArtPage.java
+++ src/interfaces/int_NewArtPage.java
@@ -3,7 +3,7 @@ package interfaces;
 public class int_NewArtPage {
 	public static final String title_texbox ="//input[@id='jform_title']";
 	public static final String category_dropdown="//select[@id='jform_catid']";
-	public static final String state_dropdown ="//select[@id='jform_state']";
+	public static final String status_dropdown ="//select[@id='jform_state']";
 	public static final String access_dropdown ="//select[@id='jform_access']";
 	public static final String save_button ="//div[@id='toolbar']//span[@class='icon-32-apply']";
 	public static final String saveclose_button="//li[@id='toolbar-save']/a/span";
